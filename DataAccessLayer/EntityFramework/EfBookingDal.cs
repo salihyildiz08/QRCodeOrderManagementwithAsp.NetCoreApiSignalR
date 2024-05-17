71b5699b64c7a249ce14bf654254e596100f2a31
Salih YILDIZ <yldzsalih27@gmail.com>
2024-05-17 15:28:56 +0300

    EntitiyFramework Classes

---
+++ DataAccessLayer/EntityFramework/EfBookingDal.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using SignalR.DataAccessLayer.Absract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfBookingDal : GenericRepository<Booking>, IBookingDal
+    {
+        public EfBookingDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
