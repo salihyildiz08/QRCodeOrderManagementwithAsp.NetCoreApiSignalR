146a05a64255b613f9b26f0041cd36ca3960e833
Salih YILDIZ <yldzsalih27@gmail.com>
2024-05-17 16:52:21 +0300

    CategoryController Api Crud

---
+++ SignalRApi/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLAyer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+      
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper) 
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _mapper.Map<List<ResultCategoryDto>>(_categoryService.TGetListAll());
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+           
+            _categoryService.TAdd(new Category()
+            {
+                CategoryName = createCategoryDto.CategoryName,
+                Status=true
+            });
+
+            return Ok("Successfull");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _categoryService.TGetByID(id);
+            _categoryService.TDelete(value);
+            return Ok("Delete Category");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            _categoryService.TUpdate(new Category()
+            {
+                CategoryID= updateCategoryDto.CategoryID,
+                Status=updateCategoryDto.Status,
+                CategoryName=updateCategoryDto.CategoryName
+            });
+            return Ok("Update Category");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _categoryService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
