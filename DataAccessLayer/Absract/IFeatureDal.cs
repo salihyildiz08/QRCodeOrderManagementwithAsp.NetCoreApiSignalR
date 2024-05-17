da8b930c06efdbe6db764efe30b1accd2a9fd636
Salih YILDIZ <yldzsalih27@gmail.com>
2024-05-17 15:13:22 +0300

    İnterfaces and GenericDal

---
+++ DataAccessLayer/Absract/IFeatureDal.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Absract
+{
+    public interface IFeatureDal : IGenericDal<Feature>
+    {
+    }
+}
