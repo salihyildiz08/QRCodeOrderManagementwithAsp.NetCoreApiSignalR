0326b10db140479641db4e6cfdab43550e4defb0
Salih YILDIZ <yldzsalih27@gmail.com>
2024-05-17 15:34:53 +0300

    Generic Service and Services classes

---
+++ BusinessLAyer/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLAyer.Abstract
+{
+    public interface IProductService : IGenericService<Product>
+    {
+    }
+}
