95a629630db326b1a03c64fe54a1816f6b78b335
Salih YILDIZ <yldzsalih27@gmail.com>
2024-05-20 10:15:48 +0300

    Layout Design

---
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task< IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7140/api/Category");
+            return View();
+        }
+    }
+}
