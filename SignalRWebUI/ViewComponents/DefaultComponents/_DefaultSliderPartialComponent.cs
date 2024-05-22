64a8949d0e80985d9be8ff97c8f05c6bdf817342
Salih YILDIZ <yldzsalih27@gmail.com>
2024-05-22 15:52:02 +0300

    List Slider

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderPartialComponent.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderPartialComponent.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderPartialComponent:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderPartialComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+      
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7140/api/Sliders");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
